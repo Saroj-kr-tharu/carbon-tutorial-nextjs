66cd320b785ed274b514f5dac0d42d3eaf93c66a
sarojkumartharu11234@gmail.com <104674219+Saroj-kr-tharu@users.noreply.github.com>
2024-12-16 22:06:56 +0545

    feat(tutorial): complete step 3

--- src/app/repos/RepoTable.js
+++ src/app/repos/RepoTable.js
@@ -14,8 +14,14 @@ import {
   TableCell,
   TableExpandedRow,
 } from '@carbon/react';
+import { Link, DataTableSkeleton, Grid, Column } from '@carbon/react';
 
 const RepoTable = ({ rows, headers }) => {
+  const getRowDescription = (rowId) => {
+    const row = rows.find(({ id }) => id === rowId);
+    return row ? row.description : '';
+  };
+
   return (
     <DataTable
       rows={rows}
@@ -51,7 +57,7 @@ const RepoTable = ({ rows, headers }) => {
                     ))}
                   </TableExpandRow>
                   <TableExpandedRow colSpan={headers.length + 1}>
-                    <p>Row description</p>
+                    <p>{getRowDescription(row.id)}</p>
                   </TableExpandedRow>
                 </React.Fragment>
               ))}
